aa50dc550be050c2413f469443b8d2564da1bd60
Holo34 <vialbeas.3@gmail.com>
2024-06-16 21:08:30 -0400

    first commit

---
+++ funciones_extra.hpp
@@ -0,0 +1,12 @@
+#ifndef FUNCIONES_EXTRA_HPP
+#define FUNCIONES_EXTRA_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "registro_cuentas.hpp"
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
